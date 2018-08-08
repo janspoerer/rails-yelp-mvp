11766602a3eaeca940f00ae22e47f05d94cf566a
Jan Spoerer <jan.spoerer@whu.edu>
2018-08-08 17:04:04 +0200

    Added routes, controllers, and views for restaurants and reviews incl. nesting

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants
+
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create, :index ]
+  end
+  resources :reviews, only: [ :show, :edit, :update, :destroy ]
+
+  root to: "restaurants_controller#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+a = Restaurant.new(name: "name1", address: "address1", phone_number: "123456789", category: "chinese")
+a.save
+
+a = Restaurant.new(name: "name2", address: "address2", phone_number: "123456789", category: "indian")
+a.save
+
+a = Restaurant.new(name: "name3", address: "address3", phone_number: "123456789", category: "french")
+a.save
+
+a = Restaurant.new(name: "name4", address: "address4", phone_number: "123456789", category: "chinese")
+a.save
+
+a = Restaurant.new(name: "name5", address: "address5", phone_number: "123456789", category: "chinese")
+a.save
